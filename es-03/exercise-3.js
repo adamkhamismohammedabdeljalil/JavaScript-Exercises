5ecd48cc697880671b4e87eeb0d521a5d64362f7
adam khamis mohammed abdeljalil <106578940+adamkhamismohammedabdeljalil@users.noreply.github.com>
2022-07-19 22:35:43 +0200

    Add files via upload

---
+++ es-03/exercise-3.js
@@ -0,0 +1,12 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  // ...
+}
+
+
+students.push('Marco');// aggiungere un elemento all'fine dell'array.
+students.unshift('Adam');// aggiungere un elemento all'inizio dell'array.
+console.log(students);
+//Un const array impedisce solo ulteriori assegnazioni ma è consentito modificarne il contenuto.
+
