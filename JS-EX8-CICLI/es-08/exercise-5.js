d19e0147f122ad664e9b86390f039147b8744bea
adam khamis mohammed abdeljalil <106578940+adamkhamismohammedabdeljalil@users.noreply.github.com>
2022-07-22 23:03:43 +0200

    Add files via upload

---
+++ JS-EX8-CICLI/es-08/exercise-5.js
@@ -0,0 +1,21 @@
+
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+let averageResult=0;
+for(let i=0;i<persons.length;i++){
+  averageResult+=persons[i].age/persons.length;
+}
+console.log(Math.round(averageResult))
+
