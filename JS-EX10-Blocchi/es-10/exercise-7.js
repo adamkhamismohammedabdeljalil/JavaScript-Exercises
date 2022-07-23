89fd1b74929c12616ad2e4bbd82ff90652ae52a3
adam khamis mohammed abdeljalil <106578940+adamkhamismohammedabdeljalil@users.noreply.github.com>
2022-07-23 11:26:29 +0200

    Add files via upload

---
+++ JS-EX10-Blocchi/es-10/exercise-7.js
@@ -0,0 +1,13 @@
+
+let maxValue=5;
+let summ=0;
+
+function sumUntil(maxValue) {
+  for(let i=0;i<maxValue;i++){
+    summ+=i;
+    console.log(summ);
+  }
+}
+
+console.log(sumUntil(5));
+console.log(summ)
