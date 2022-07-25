254e330d429cb6aeae16c626ddb3e0ac4d113e0b
adam khamis mohammed abdeljalil <106578940+adamkhamismohammedabdeljalil@users.noreply.github.com>
2022-07-25 22:45:19 +0200

    Add files via upload

---
+++ JS-EX28-Optional chaining2/es-28/exercise-2.js
@@ -0,0 +1,8 @@
+const order = {};
+
+if (order && order.customer && order.customer.address && !order.customer.address.city) {
+  console.log('City is required');
+}
+
+let city=order?.customer?.address?.city;
+console.log(city)
