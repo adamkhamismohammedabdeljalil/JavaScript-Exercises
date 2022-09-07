2cbcad0304055cbfc1ee8ec6c8bf09a1889eb40b
adam khamis mohammed abdeljalil <106578940+adamkhamismohammedabdeljalil@users.noreply.github.com>
2022-09-07 20:36:35 +0200

    Add files via upload

--- JS-EX48-Destructuring assignment/exercise-5.js
+++ JS-EX48-Destructuring assignment/exercise-5.js
@@ -5,5 +5,5 @@ const person = {
   age: 25
 };
 
-const { id, name='Mario', surname='Rossi', old=25 } = person;
+const { id, firstName:name, lastName:surname, age:old} = person;
 console.log(id, name, surname, old);
